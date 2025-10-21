c46d36f5963d38b9a6f2fe533bd8b1bed4404004
sayo <kyrbynyt@gmail.com>
2025-10-21 20:59:41 +0300

    font is rendering (hopefully)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use cgmath::SquareMatrix;
+use image::EncodableLayout;
 use wgpu::util::DeviceExt;
 
 fn main() {
@@ -71,6 +72,14 @@ pub struct Vertex {
     color: [f32; 3],
 }
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct FontVertex {
+    pos: [f32; 3],
+    color: [f32; 3],
+    texture_coords: [f32; 2],
+}
+
 impl Vertex {
     pub fn desc() -> wgpu::VertexBufferLayout<'static> {
         wgpu::VertexBufferLayout {
@@ -92,6 +101,31 @@ impl Vertex {
     }
 }
 
+impl FontVertex {
+    pub fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<FontVertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 6]>() as wgpu::BufferAddress,
+                    shader_location: 2,
+                    format: wgpu::VertexFormat::Float32x2,
+                },
+            ],
+        }
+    }
+}
 
 struct Camera {
     w: f32,
@@ -148,6 +182,131 @@ struct Renderer {
 
     draw_vertices: Vec<Vertex>,
     draw_indices: Vec<u16>,
+
+    font_atlas: MonoGlyphAtlas,
+    font_bind_group: wgpu::BindGroup,
+
+    font_draw_vertices: Vec<FontVertex>,
+    font_draw_indices: Vec<u16>,
+}
+
+pub struct MonoGlyphAtlas {
+    pub texture: wgpu::Texture,
+    pub view: wgpu::TextureView,
+    pub sampler: wgpu::Sampler,
+    pub glyph_map: std::collections::HashMap<char, (f32, f32, f32, f32)>,
+    pub cell_size: (u32, u32),
+}
+
+pub fn create_monospace_atlas(
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+    font_data: &[u8],
+    scale: f32,
+) -> MonoGlyphAtlas {
+    use ab_glyph::Font;
+    let font = ab_glyph::FontRef::try_from_slice(font_data).unwrap();
+    let scale = ab_glyph::PxScale::from(scale);
+
+    let chars: Vec<char> = (0x20u8..0x7Fu8).map(|c| c as char).collect();
+
+    let test_glyph = font
+        .outline_glyph(font.glyph_id('M').with_scale(scale))
+        .unwrap();
+    let bb = test_glyph.px_bounds();
+    let cell_w = bb.width().ceil() as u32;
+    let cell_h = bb.height().ceil() as u32;
+
+    let cols = 16;
+    let rows = ((chars.len() as f32) / cols as f32).ceil() as u32;
+    let atlas_width = cols * cell_w;
+    let atlas_height = rows * cell_h;
+
+    let mut atlas = image::RgbaImage::new(atlas_width, atlas_height);
+    let mut glyph_map = std::collections::HashMap::new();
+
+    for (i, &ch) in chars.iter().enumerate() {
+        let glyph = font.glyph_id(ch).with_scale(scale);
+        if let Some(og) = font.outline_glyph(glyph) {
+            let mut img = image::RgbaImage::new(cell_w, cell_h);
+            let glyph_bb = og.px_bounds();
+
+            let x_off = ((cell_w as f32 - glyph_bb.width()) / 2.0).floor() as i32;
+            let y_off = ((cell_h as f32 - glyph_bb.height()) / 2.0).floor() as i32;
+
+            og.draw(|x, y, v| {
+                let px = (x as i32 + x_off).max(0) as u32;
+                let py = (y as i32 + y_off).max(0) as u32;
+                if px < cell_w && py < cell_h {
+                    img.put_pixel(px, py, image::Rgba([255, 255, 255, (v * 255.0) as u8]));
+                }
+            });
+
+            let x = (i as u32 % cols) * cell_w;
+            let y = (i as u32 / cols) * cell_h;
+
+            image::imageops::overlay(&mut atlas, &img, x.into(), y.into());
+
+            let u0 = x as f32 / atlas_width as f32;
+            let v0 = y as f32 / atlas_height as f32;
+            let u1 = (x + cell_w) as f32 / atlas_width as f32;
+            let v1 = (y + cell_h) as f32 / atlas_height as f32;
+            glyph_map.insert(ch, (u0, v0, u1, v1));
+        }
+    }
+
+    let texture = device.create_texture(&wgpu::TextureDescriptor {
+        label: Some("Monospace Glyph Atlas"),
+        size: wgpu::Extent3d {
+            width: atlas_width,
+            height: atlas_height,
+            depth_or_array_layers: 1,
+        },
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: wgpu::TextureFormat::Rgba8UnormSrgb,
+        usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+        view_formats: &[],
+    });
+
+    queue.write_texture(
+        wgpu::TexelCopyTextureInfo {
+            texture: &texture,
+            mip_level: 0,
+            origin: wgpu::Origin3d::ZERO,
+            aspect: wgpu::TextureAspect::All,
+        },
+        atlas.as_bytes(),
+        wgpu::TexelCopyBufferLayout {
+            offset: 0,
+            bytes_per_row: Some(4 * atlas_width),
+            rows_per_image: Some(atlas_height),
+        },
+        wgpu::Extent3d {
+            width: atlas_width,
+            height: atlas_height,
+            depth_or_array_layers: 1,
+        },
+    );
+
+    let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+    let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+        label: Some("Glyph Sampler"),
+        address_mode_u: wgpu::AddressMode::ClampToEdge,
+        address_mode_v: wgpu::AddressMode::ClampToEdge,
+        mag_filter: wgpu::FilterMode::Linear,
+        min_filter: wgpu::FilterMode::Linear,
+        ..Default::default()
+    });
+
+    MonoGlyphAtlas {
+        texture,
+        view,
+        sampler,
+        glyph_map,
+        cell_size: (cell_w, cell_h),
+    }
 }
 
 impl Renderer {
@@ -170,8 +329,6 @@ impl Renderer {
         let capabilities = surface.get_capabilities(&adapter);
 
         let surface_fmt = capabilities.formats[0];
-
-        let quad_shader = device.create_shader_module(wgpu::include_wgsl!("quad_shader.wgsl"));
 
         // camera setup begin
         let camera = Camera {
@@ -213,6 +370,7 @@ impl Renderer {
         // camera setup end
 
         // quad renderer pipeline
+        let quad_shader = device.create_shader_module(wgpu::include_wgsl!("quad_shader.wgsl"));
         let quad_render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: None,
@@ -259,6 +417,108 @@ impl Renderer {
         });
 
         // font setup
+        let font = include_bytes!("iosevka-regular.ttf");
+        let atlas = create_monospace_atlas(&device, &queue, font, 32.0);
+
+        let font_shader = device.create_shader_module(wgpu::include_wgsl!("font_shader.wgsl"));
+        let font_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: None,
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::VERTEX,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            multisampled: false,
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 2,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                ],
+            });
+
+        let font_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &font_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::Buffer(
+                        camera_buffer.as_entire_buffer_binding(),
+                    ),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::TextureView(&atlas.view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: wgpu::BindingResource::Sampler(&atlas.sampler),
+                },
+            ],
+            label: Some("diffuse_bind_group"),
+        });
+
+        let font_render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts: &[&font_bind_group_layout],
+                push_constant_ranges: &[],
+            });
+
+        let font_render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&font_render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &font_shader,
+                entry_point: Some("vs_main"),
+                buffers: &[FontVertex::desc()],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            },
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Cw,
+                cull_mode: None,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &font_shader,
+                entry_point: Some("fs_main"),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: surface_fmt,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            }),
+            multiview: None,
+            cache: None,
+        });
 
         let renderer = Self {
             window,
@@ -277,13 +537,21 @@ impl Renderer {
             size,
             surface,
             surface_fmt,
+
             quad_render_pipeline,
+
             camera,
             camera_uniform,
             camera_buffer,
             camera_bind_group,
+
             draw_indices: vec![],
             draw_vertices: vec![],
+
+            font_atlas: atlas,
+            font_bind_group,
+            font_draw_indices: vec![],
+            font_draw_vertices: vec![],
         };
 
         renderer.configure_surface();
@@ -344,7 +612,8 @@ impl Renderer {
                 });
             self.vbo = vbo;
         } else {
-            self.queue.write_buffer(&self.vbo, 0, bytemuck::cast_slice(&self.draw_vertices));
+            self.queue
+                .write_buffer(&self.vbo, 0, bytemuck::cast_slice(&self.draw_vertices));
         }
 
         if (self.ibo.size() as usize) < self.draw_indices.len() * std::mem::size_of::<u16>() {
@@ -358,10 +627,9 @@ impl Renderer {
                 });
             self.ibo = ibo;
         } else {
-            self.queue.write_buffer(&self.ibo, 0, bytemuck::cast_slice(&self.draw_indices));
-
+            self.queue
+                .write_buffer(&self.ibo, 0, bytemuck::cast_slice(&self.draw_indices));
         }
-
     }
 
     pub fn render(&mut self) {
